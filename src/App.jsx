7524e2d7dc5be04de7d58bf1bedf38332351c870
chavimiller <chaviweisman@gmail.com>
2025-10-21 12:09:32 +0300

    Create projects page component and project card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import About from "./components/About";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <>
       <About />
+      <Projects />
     </>
   );
 }
